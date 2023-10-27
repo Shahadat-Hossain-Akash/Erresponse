21242a49801c8962d9dad7055a2389b9d8b610f9
Shahadat <shahadat.akash007@gmail.com>
2023-10-27 23:31:48 +0600

    minor fix

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,10 +1,7 @@
-import NewIssue from "@/components/NewIssue";
-import delay from "delay";
 import React from "react";
 import IssueForm from "../_components/IssueForm";
 
 const NewIssuePage = async () => {
-  await delay(3000);
   return (
     <div className="flex w-full">
       <IssueForm />
