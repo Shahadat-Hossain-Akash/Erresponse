8079d77318029716c9b3bf3f44ee5779123bc950
Shahadat <shahadat.akash007@gmail.com>
2023-10-28 03:48:07 +0600

    optimization

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,7 +1,15 @@
 import prisma from '@/prisma/client';
 import { NextRequest, NextResponse } from 'next/server';
+import { getServerSession } from "next-auth";
+import authOptions from "@/app/auth/authOptions";
 
 export async function GET(request:NextRequest) {
+
+    const session = await getServerSession(authOptions)
+
+    if(!session) 
+        return NextResponse.json("Access denied", {status: 401})
+
     const users = await prisma.user.findMany({orderBy: {name: 'asc'}})
 
     return NextResponse.json(users, {status:200})
